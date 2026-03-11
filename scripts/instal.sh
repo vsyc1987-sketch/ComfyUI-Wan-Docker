4f3c1fbae802a5861ea1b5035302018dcbf5182c
Vadim Sychev <vsyc1987@gmail.com>
2026-03-11 12:26:58 +0200

    instal.sh

--- scripts/instal.sh
+++ scripts/instal.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# Автоматически находим, где лежит ComfyUI
-CPATH=$(find /workspace -name "ComfyUI" -type d | head -n 1)
+# 1. Создаем папки
+mkdir -p /workspace/ComfyUI/models/diffusion_models/
+mkdir -p /workspace/ComfyUI/models/vae
+mkdir -p /workspace/ComfyUI/models/text_encoders/
 
-# Создаем папки там, где они должны быть
-mkdir -p "$CPATH/models/diffusion_models"
-mkdir -p "$CPATH/models/text_encoders"
-mkdir -p "$CPATH/models/vae"
+# 2. Скачиваем модели (Здесь ВАЖНО: $ перед HF_TOKEN)
+echo "Downloading Models..."
 
-echo "Starting download into $CPATH..."
+curl -L -H "Authorization: Bearer $HF_TOKEN" -o /workspace/ComfyUI/models/vae/wan_2.1_vae.safetensors "https://huggingface.co/vsyc1987/wan_2.1_vae.safetensors/resolve/main/wan_2.1_vae.safetensors"
 
-# Качаем модели (те самые стрелочки появятся тут)
-curl -L -H "Authorization: Bearer $HF_TOKEN" -o "$CPATH/models/vae/wan_2.1_vae.safetensors" "https://huggingface.co/vsyc1987/wan_2.1_vae.safetensors/resolve/main/wan_2.1_vae.safetensors"
+curl -L -H "Authorization: Bearer $HF_TOKEN" -o /workspace/ComfyUI/models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors "https://huggingface.co/vsyc1987/umt5_xxl_fp8_e4m3fn_scaled.safetensors/resolve/main/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 
-curl -L -H "Authorization: Bearer $HF_TOKEN" -o "$CPATH/models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors" "https://huggingface.co/vsyc1987/umt5_xxl_fp8_e4m3fn_scaled.safetensors/resolve/main/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+curl -L -H "Authorization: Bearer $HF_TOKEN" -o /workspace/ComfyUI/models/unet/Artius-Wan22-14b-I2V-high-Q4_K_M-v2.gguf "https://huggingface.co/vsyc1987/Artius-Wan22-14b-I2V-high-Q4_K_M-v2.gguf/resolve/main/Artius-Wan22-14b-I2V-high-Q4_K_M-v2.gguf"
 
-curl -L -H "Authorization: Bearer $HF_TOKEN" -o "$CPATH/models/diffusion_models/Artius-Wan22-14b-I2V-high-Q4_K_M-v2.gguf" "https://huggingface.co/vsyc1987/Artius-Wan22-14b-I2V-high-Q4_K_M-v2.gguf/resolve/main/Artius-Wan22-14b-I2V-high-Q4_K_M-v2.gguf"
+curl -L -H "Authorization: Bearer $HF_TOKEN" -o /workspace/ComfyUI/models/unet/Artius-Wan22-14b-I2V-low-Q4_K_M-v2.gguf "https://huggingface.co/vsyc1987/Artius-Wan22-14b-I2V-low-Q4_K_M-v2.gguf/resolve/main/Artius-Wan22-14b-I2V-low-Q4_K_M-v2.gguf"
 
-echo "Done!"
+# 3. Библиотеки
+pip install psutil nvidia-ml-py3
+
+# 4. Автозагрузка воркфлоу
+mkdir -p /workspace/ComfyUI/user/default_workflows
+cp /workspace/vsyс-sketch/presets/*.json /workspace/ComfyUI/user/default_workflows/
+cp /workspace/vsyс-sketch/presets/Artius_wan2_2_14B_flf2v.json /workspace/ComfyUI/web/scripts/defaultGraph.json
+
+echo "All done! Launching ComfyUI..."
