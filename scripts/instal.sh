9efc336ad7d44b414682cfe399c07c4ea5b1fb87
Vadim Sychev <vsyc1987@gmail.com>
2026-03-12 11:45:44 +0200

    Update instal.sh

--- scripts/instal.sh
+++ scripts/instal.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# 1. Находим реальный путь к ComfyUI
+# Создаем папку в самом начале для проверки
+mkdir -p /workspace/VADIM_ALIVE
+
+# Находим путь к ComfyUI
 CPATH=$(find /workspace -name "ComfyUI" -type d | head -n 1)
 
-# 2. Создаем нужные папки
+# Создаем папки для моделей и твою личную
 mkdir -p "$CPATH/models/diffusion_models"
 mkdir -p "$CPATH/models/vae"
 mkdir -p "$CPATH/models/text_encoders"
 mkdir -p "$CPATH/models/vadim_folder"
 
-echo "=== STARTING DOWNLOAD INTO $CPATH ==="
+echo "=== STARTING DOWNLOAD ==="
 
-# VAE
+# Загрузка VAE
 wget --header="Authorization: Bearer $HF_TOKEN" -O "$CPATH/models/vae/wan_2.1_vae.safetensors" --progress=bar:force "https://huggingface.co/vsyc1987/wan_2.1_vae.safetensors/resolve/main/wan_2.1_vae.safetensors" 2>&1
 
-# Text Encoder
+# Загрузка Text Encoder
 wget --header="Authorization: Bearer $HF_TOKEN" -O "$CPATH/models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors" --progress=bar:force "https://huggingface.co/vsyc1987/umt5_xxl_fp8_e4m3fn_scaled.safetensors/resolve/main/umt5_xxl_fp8_e4m3fn_scaled.safetensors" 2>&1
 
 # Wan 2.1 HIGH
@@ -23,8 +26,8 @@ wget --header="Authorization: Bearer $HF_TOKEN" -O "$CPATH/models/diffusion_mode
 # Wan 2.1 LOW
 wget --header="Authorization: Bearer $HF_TOKEN" -O "$CPATH/models/diffusion_models/Artius-Wan22-14b-I2V-low-Q4_K_M-v2.gguf" --progress=bar:force "https://huggingface.co/vsyc1987/Artius-Wan22-14b-I2V-low-Q4_K_M-v2.gguf/resolve/main/Artius-Wan22-14b-I2V-low-Q4_K_M-v2.gguf" 2>&1
 
-# 3. Установка менеджера Смышникова
+# Менеджер Смышникова
 cd "$CPATH/custom_nodes"
 git clone https://github.com/smisnikov/comfyui-preset-download-manager
 
-echo "All done! Launching ComfyUI..."
+echo "=== ALL DONE ==="
