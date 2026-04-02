31b4b431fa09531f85caccacd5e12b0501d2bb65
Vadim Sychev <vsyc1987@gmail.com>
2026-04-02 19:46:00 +0300

    Update pre_start.sh

--- scripts/pre_start.sh
+++ scripts/pre_start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Переходим в корень ComfyUI
-cd /workspace/ComfyUI
-
-# ПОДМЕНА ВОРКФЛОУ (Секрет Смышникова)
+# 1. Подмена воркфлоу (Самое важное из FAQ)
 if [ -f "/workspace/user_workflows/workflow.json" ]; then
-    echo "!!! [REPORT] Copying workflow to default..."
-    cp /workspace/user_workflows/workflow.json ./web/scripts/default_workflow.json
+    echo "!!! [REPORT] Applying your custom workflow as default..."
+    cp /workspace/user_workflows/workflow.json /workspace/web/scripts/default_workflow.json
 fi
 
-# Установка менеджера (если нужно)
-cd custom_nodes
+# 2. Установка менеджера
+cd /workspace/custom_nodes
 if [ ! -d "ComfyUI-Manager" ]; then
     git clone https://github.com/ltdrdata/ComfyUI-Manager.git
 fi
